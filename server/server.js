0db9d3296005c799705eac6af332716298b5b0a1
yoondabin <yundabin0608@gmail.com>
2021-11-07 03:08:34 +0900

    SUCCESS Fetch

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+app.use(express.json())
 const cors = require('cors');
 const port =3001;
 const route = require('./routes/index');
 
 app.use(cors());
 app.use('/', route);
+
 // 연결하려는 주소와 라우터 연결하기
 
 app.listen(port, ()=>{
--- src/Components/SelectLang.js
+++ src/Components/SelectLang.js
@@ -3,15 +3,15 @@ import "../css/SelectPage.css";
 const API_URL = 'http://localhost:3001'
 
 function SelectLang (){
-    const [lanuage, setLanguage]=useState(null);
+    const [language, setLanguage]=useState(null);
     const page=2;
     var inform;
 
     // usecallback 써서 성능최적화 시키던가
-    const onClickHandler = (lang)=>{
+    const onClickHandler = async (lang)=>{
         setLanguage(lang);
-        inform = {lanuage,page};
-
+        
+        inform = {language: language};
         fetch(`${API_URL}/language`, {
             method: 'POST',
             headers: {
@@ -19,10 +19,18 @@ function SelectLang (){
             },
             body: JSON.stringify(inform),  
         })
-        .then(response => console.log(JSON.stringify(inform)))
+        .then(async res => { 
+            try {
+                const jsonRes = await res.json();
+                if (res.status === 200) {
+                    console.log(jsonRes.message);
+                }
+            } catch (err) {
+                console.log(err);
+            };
+        });
     }
    
-
     // language가 Null이 아니면 화살표 활성화하는 함수 (근데 수행 타이밍을 못찾겠음 - 변수가 바뀔때마다 검사하게 하는건 ㅇㄸ)
     // <Link to="/maxlength"></Link>
     
@@ -44,7 +52,4 @@ function SelectLang (){
     )
     
 }
-
-// onMouseOver={console.log("!!")}
-// onMouseLeave={console.log("!!")}
 export default SelectLang;
