f3f366ef7253bf6374afcf444a05e949794632bf
yoondabin <yundabin0608@gmail.com>
2021-11-06 13:31:05 +0900

    CREATE SERVER

--- public/server/server.js
+++ public/server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const port =3001;
 
 app.use(cors());
-app.use('/api', (req, res)=> res.json({username:'bryan'}));
+app.use('/api', (req, res)=> res.json({username:'SOOKMYUNG'}));
 
 app.listen(port, ()=>{
     console.log(`express is running on ${port}`);
