e8983e962a9eef6ef67d16398079de796119b8d8
yoondabin <yundabin0608@gmail.com>
2021-11-07 00:35:17 +0900

    Route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import './App.css';
-
+import React from 'react';
 import StartPage from './Components/StartPage';
 import ResultPage from './Components/ResultPage';
+import SelectAbout from './Components/SelectAbout';
+import SelectNum from './Components/SelectNum';
+import SelectLang  from './Components/SelectLang';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 
-function App() {
+const App = ()=>{
   return (
-    <BrowserRouter>
-    
-    <StartPage/>
+    <div>
+      <BrowserRouter>
       <Routes>
-        
-        <Route path="/" exact component={StartPage}/>
-        <Route path="/result" component={ResultPage}>
-
-        </Route>
+        <Route exact path="/" element={<StartPage />}/>
+        <Route path="/language" element={<SelectLang/>}/>
+        <Route path="/maxlen" element={<SelectNum/>}/>
+        <Route path="/about" elementt={<SelectAbout/>}/>
+        <Route path="/result" element={<ResultPage/>}/>
       </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </div>
+  
   );
 }
 
