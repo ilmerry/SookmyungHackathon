460b9694e4195f17934e4dc6980f3263ac9bd592
ilmerry <milkiss0414@naver.com>
2021-11-06 19:44:31 +0900

    Select Page

--- src/Pages/SelectPage.js
+++ src/Pages/SelectPage.js
@@ -2,11 +2,20 @@ import React from "react";
 import Progressbar from "../Components/Progressbar";
 import SelectLang from "../Components/SelectLang";
 import SelectNum from "../Components/SelectNum";
+import SelectAbout from "../Components/SelectAbout";
+
+const backgroundStyle = {
+        backgroundImage: 'url(/images/selectLangBackground.png)',
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh"
+}
 
 function SelectPage(){
     return (
-        <div>
+        <div style={backgroundStyle}>
             <Progressbar />
+            <SelectNum />
         </div>
     );
 }
